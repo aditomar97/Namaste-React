683fabc6a28fceee66e8ef7d66e9e16be13d0ca8
aditomar97 <adityatomar.singh97@gmail.com>
2023-12-25 18:48:56 +0530

    Inital Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent= React.createElement("div",{},
+[React.createElement("div",{},React.createElement("h1",{},"I am a h1 tag")),
+React.createElement("div",{},React.createElement("h1",{},"Hello am a h2 tag"))
+
+]
+
+)
+
+const root =ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
