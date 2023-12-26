42e52199e5cd9ab5413ae1993f10c6a984e8f143
aditomar97 <adityatomar.singh97@gmail.com>
2023-12-26 09:32:44 +0530

    Igniting app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const parent= React.createElement("div",{},
 [React.createElement("div",{},React.createElement("h1",{},"I am a h1 tag")),
 React.createElement("div",{},React.createElement("h1",{},"Hello am a h2 tag"))
