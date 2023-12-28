2668b27242c28eab419abf2f93794514b0f72ea1
aditomar97 <adityatomar.singh97@gmail.com>
2023-12-28 13:22:42 +0530

    Lecture no third

--- App.js
+++ App.js
@@ -21,11 +21,11 @@ const Header = () => {
     </div>
   );
 };
-const RestaurantCard=()=>{
+const RestaurantCard=({resname})=>{
     return(
         <div className="res-card">
             <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/e0vvulfbahjxjz6k4uwi"/>
-            <h3>Meghana Foods</h3>
+            <h3>{resname}</h3>
             <h4>Biryani,North Indina,Asian</h4>
             <h4>4.4 Star Ratings</h4>
             <h4>38 minutes</h4>
@@ -37,19 +37,9 @@ const Body = () => {
     <div className="Body">
       <div className="Search">Search</div>
       <div className="res-container">
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
-       <RestaurantCard/>
+       <RestaurantCard resname="Meghana Foods"/>
+       <RestaurantCard resname="KFC"/>
+      
       </div>
     </div>
   );
