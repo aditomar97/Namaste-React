44eb713b0c0a93d0c79ec02179fad1e2d8ff96a2
aditomar97 <adityatomar.singh97@gmail.com>
2023-12-28 13:04:24 +0530

    UI Base

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import "./index.css";
 const Header = () => {
   return (
     <div className="header">
@@ -10,13 +10,47 @@ const Header = () => {
           src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
         />
       </div>
-      <div className="nav-items"></div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard=()=>{
+    return(
+        <div className="res-card">
+            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/e0vvulfbahjxjz6k4uwi"/>
+            <h3>Meghana Foods</h3>
+            <h4>Biryani,North Indina,Asian</h4>
+            <h4>4.4 Star Ratings</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+const Body = () => {
+  return (
+    <div className="Body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+      </div>
     </div>
   );
 };
@@ -25,6 +59,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body/>
     </div>
   );
 };
